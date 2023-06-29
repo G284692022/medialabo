f4f84efeff66b0633a5e044ac679ee3156acff5e
G284692022 <228469@st.takushoku-u.ac.jp>
2023-06-29 15:05:17 +0900

    1

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -16,10 +16,10 @@ let data = [
 // 練習4-2 メッセージ追加プログラム
 let h1=document.querySelector('h2#ex42');
 
-
-// p.textContent='写真表と都市の緯度経度のページです'; 
-// h1.insertAdjacentElement('afterend', p);
-// p.style.textEmphasis='sesami green'; 
+let p=document.createElement('p'); ;
+p.textContent='写真表と都市の緯度経度のページです'; 
+h1.insertAdjacentElement('afterend', p);
+p.style.textEmphasis='sesami green'; 
 
 // 練習4-3 写真表作成プログラム
 
